c108393f200e50d573b9eb4980bf986715d3ee8b
name <ebammo20@gmail.com>
2019-12-25 09:09:52 +0330

    modifying regs, selecting an image, proper server name. creating server

--- mtph/views.py
+++ mtph/views.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import random
 import time
@@ -32,8 +33,9 @@ class DoHandler(digitalocean.Manager):
         my_droplets = self.get_all_droplets()
         droplets = [{
             'id': droplet.id,
+            'tags': droplet.tags,
             'ip': droplet.ip_address,
-        } for droplet in my_droplets]
+        } for droplet in my_droplets if 'api' not in droplet.tags]
 
         return droplets
 
@@ -45,19 +47,38 @@ class DoHandler(digitalocean.Manager):
         return drop
 
     def create_new_droplet(self):
-        avail_regs = ['ams3', 'fra1', 'lon1', 'nyc3', 'sfo1']
+        avail_regs = ['ams3', 'fra1', 'lon1', 'nyc1', 'nyc3', 'tor1', 'sfo2']
         max_size, avail_regs_raw = self.get_sizes()
         my_images = self.get_my_images()
-        image_id = my_images[0].id
+        image = my_images[-1]
+
+        for a in my_images:
+            if a.name == 'mtpserver16g':
+                image = a
+
         region = random.choice(avail_regs)
-        print(region)
+        print('first reg: ', region)
         while region not in avail_regs_raw:
             region = random.choice(avail_regs)
-            print(region)
-        obj = digitalocean.Droplet(token=self.token, name='auto-generated', region=region, size=max_size,
+            print('loop reg: ', region)
+
                                    image=image_id)
+        creation_time = datetime.datetime.now()
+        time_string = \
+            str(creation_time.year) + \
+            str(creation_time.month) + \
+            str(creation_time.day) + '-' + \
+            str(creation_time.hour) + \
+            str(creation_time.minute) + \
+            str(creation_time.second)
+        obj = digitalocean.Droplet(token=self.token, name='ag-' + time_string, region=region, size=max_size,
+                                   image=image.id)
+        print('start creating.')
         obj.create()
-        print('Creating new droplet!')
+        print('new droplet!', obj.ip_address)
+        # except:
+        #     self.create_new_droplet()
+        print('start waiting')
         time.sleep(30)
 
         return obj
@@ -97,14 +118,18 @@ def change_server(request):
     print('fingind by id: ', id)
     do_handler = DoHandler(token)
     old_droplet = do_handler.get_droplet_by_id(id)
-    # old_droplet.destroy()
-    print('destroying old droplet! => ', old_droplet.ip_address)
-    time.sleep(15)
+    old_ip = old_droplet.ip_address
+    print('destroying old droplet! => ', old_ip)
+    old_droplet.destroy()
+    time.sleep(20)
     new_drop = do_handler.create_new_droplet()
+    new_drop = do_handler.get_droplet_by_id(new_drop.id)
+    print('new drop created...: ', new_drop.ip_address)
     context = {
         'id': new_drop.id,
         'ip': new_drop.ip_address,
     }
+    print(context)
+
+
     return JsonResponse(context, safe=False)
-
-
