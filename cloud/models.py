c1ed47161b1576e1e818e3351e0aef141afcb5ed
name <ebammo20@gmail.com>
2020-01-02 19:11:46 +0330

    adding cloud model fields to s and c models

--- cloud/models.py
+++ cloud/models.py
@@ -24,6 +24,7 @@ class Cloud(TimeStampedModel):
     secret = models.CharField(max_length=100)
     access = models.CharField(max_length=100, blank=True, null=True)
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s: %s owned by: %s' % (self.type.name.capitalize(), self.name.capitalize(), self.owner.username.capitalize())
@@ -32,11 +33,11 @@ class Cloud(TimeStampedModel):
 class Server(TimeStampedModel):
     cloud = models.ForeignKey(Cloud, on_delete=models.CASCADE)
     name = models.CharField(max_length=30, blank=True, null=True)
+    server_id = models.CharField(max_length=30)
     ipv4 = models.CharField(max_length=20)
     dns = models.CharField(max_length=200, blank=True, null=True)
     fail = models.BooleanField(default=False)
+    type = models.CharField(max_length=10, default='front')
 
     def __str__(self):
-        return 'IP: %s - %s - %s' % (self.ipv4, self.cloud.type.name.capitalize(), self.cloud.owner.username.capitalize())
-
-
+        return '%s - %s - %s - %s -  %s - %s' % (self.cloud.owner.username.capitalize(), self.cloud.name.capitalize(), self.cloud.type.name.capitalize(), self.server_id, self.ipv4, self.type)
