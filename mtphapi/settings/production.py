a08d196b7df9b3a068c73a5b94cf87f8a4610ed1
name <ebammo20@gmail.com>
2020-01-16 20:07:25 +0330

    modifying production allowed_hosts

--- mtphapi/settings/production.py
+++ mtphapi/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 
-ALLOWED_HOSTS = ['165.227.65.164']
+ALLOWED_HOSTS += ['165.227.65.164', '3.84.206.7']
 SECRET_KEY = set_secret_key(
     os.environ['DJANGO_SETTINGS_MODULE'],
     'SECRET_KEY_PRODUCTION'
